10028f3ece5f926db1eb22011886676c641e530b
Dimitris Karagkasidis <t.pagef.lt@gmail.com>
2018-08-17 00:45:28 +0200

    Convert tabs to spaces

--- jailventory.py
+++ jailventory.py
@@ -23,49 +23,49 @@ groups = {
 }
 
 def compile_inventory():
-	# Compile an inventory of running jails by quering jls(8). 
-	try:
-		Jail = namedtuple('Jail', 'jid ip_address hostname path'.split())
-		for j in check_output(['jls']).split("\n")[1:-1]:
-			jail = Jail(*j.split())
-			groups['jails']['hosts'].append(jail.hostname)
-			hosts[jail.hostname] = { 'ansible_host': jail.ip_address }
+    # Compile an inventory of running jails by quering jls(8). 
+    try:
+        Jail = namedtuple('Jail', 'jid ip_address hostname path'.split())
+        for j in check_output(['jls']).split("\n")[1:-1]:
+            jail = Jail(*j.split())
+            groups['jails']['hosts'].append(jail.hostname)
+            hosts[jail.hostname] = { 'ansible_host': jail.ip_address }
 
-	except Exception as Ex:
-		raise Exception("get_jails(): %s" % Ex)
+    except Exception as Ex:
+        raise Exception("get_jails(): %s" % Ex)
 
-	
+    
 def parse_args():
-	# Parse command-line arguments.
-	try:
-		parser = ArgumentParser(description='Ansible dynamic inventory \
-									  		 provider for FreeBSD jails')
-		group = parser.add_mutually_exclusive_group(required=True)
-		group.add_argument("--list", action="store_true")
-		group.add_argument("--host", metavar='HOST', type=str)
-		return parser.parse_args()
+    # Parse command-line arguments.
+    try:
+        parser = ArgumentParser(description='Ansible dynamic inventory \
+                                             provider for FreeBSD jails')
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument("--list", action="store_true")
+        group.add_argument("--host", metavar='HOST', type=str)
+        return parser.parse_args()
 
-	except Exception as Ex:
-		raise Exception("parse_args(): %s" % Ex)
+    except Exception as Ex:
+        raise Exception("parse_args(): %s" % Ex)
 
 
 def main():
-	try:
-		args = parse_args()
-		compile_inventory()
+    try:
+        args = parse_args()
+        compile_inventory()
 
-		if args.list:
-			sys.stdout.write(dumps(groups))
-		else:
-			sys.stdout.write(dumps(hosts[args.host]) if args.host in hosts
-													 else "{}")
-		return 0
+        if args.list:
+            sys.stdout.write(dumps(groups))
+        else:
+            sys.stdout.write(dumps(hosts[args.host]) if args.host in hosts
+                                                     else "{}")
+        return 0
 
-	except Exception as Ex:
-		sys.stderr.write("error: %s" % Ex)
-		return 1
+    except Exception as Ex:
+        sys.stderr.write("error: %s" % Ex)
+        return 1
 
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
 
