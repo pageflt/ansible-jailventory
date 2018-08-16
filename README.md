6111ba9d34e8ba66b50e4dcd224907b9a5aeede8
Dimitris Karagkasidis <t.pagef.lt@gmail.com>
2018-08-17 00:39:56 +0200

    Update README and commit the script

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# freebsd-vps-ops
+# jailventory
+
+Ansible dynamic inventory provider for FreeBSD jails.
+
+## Usage
+
+- Make sure `jailventory.py` is executable:
+
+```
+$ chmod +x jailventory.py
+```
+
+- Add the following directive to your `ansible.cfg`:
+
+```
+inventory = /path/to/jailventory.py
+```
+
+- Ansible should now automatically detect any active jails and add them to the `jails` group:
+
+```
+$ jls
+   JID  IP Address      Hostname                      Path
+    16  192.168.1.1     www                           /home/jails/www
+    17  192.168.1.2     dns                           /home/jails/dns
+$ ansible jails -m ping -o
+dns | SUCCESS => {"changed": false, "ping": "pong"}
+www | SUCCESS => {"changed": false, "ping": "pong"}
+$ sudo service jail start mail                                                                                                           
+Starting jails: mail.
+$ ansible jails -m ping -o
+dns | SUCCESS => {"changed": false, "ping": "pong"}
+www | SUCCESS => {"changed": false, "ping": "pong"}
+mail | SUCCESS => {"changed": false, "ping": "pong"}
+```
+
---
+++ jailventory.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python2.7
+
+# Ansible dynamic inventory provider for FreeBSD jails. Details:
+# https://docs.ansible.com/ansible/2.6/dev_guide/developing_inventory.html
+#
+# Dimitris Karagkasidis, <t.pagef.lt@gmail.com>
+# https://github.com/pageflt/freebsd-vps-ops
+
+import sys
+from json import dumps
+from argparse import ArgumentParser
+from subprocess import check_output
+from collections import namedtuple
+
+hosts = {}
+groups = {
+  "jails": {
+    "hosts": [],
+    "vars": {
+      "ansible_python_interpreter": "/usr/local/bin/python2.7",
+    }
+  }
+}
+
+def compile_inventory():
+	# Compile an inventory of running jails by quering jls(8). 
+	try:
+		Jail = namedtuple('Jail', 'jid ip_address hostname path'.split())
+		for j in check_output(['jls']).split("\n")[1:-1]:
+			jail = Jail(*j.split())
+			groups['jails']['hosts'].append(jail.hostname)
+			hosts[jail.hostname] = { 'ansible_host': jail.ip_address }
+
+	except Exception as Ex:
+		raise Exception("get_jails(): %s" % Ex)
+
+	
+def parse_args():
+	# Parse command-line arguments.
+	try:
+		parser = ArgumentParser(description='Ansible dynamic inventory \
+									  		 provider for FreeBSD jails')
+		group = parser.add_mutually_exclusive_group(required=True)
+		group.add_argument("--list", action="store_true")
+		group.add_argument("--host", metavar='HOST', type=str)
+		return parser.parse_args()
+
+	except Exception as Ex:
+		raise Exception("parse_args(): %s" % Ex)
+
+
+def main():
+	try:
+		args = parse_args()
+		compile_inventory()
+
+		if args.list:
+			sys.stdout.write(dumps(groups))
+		else:
+			sys.stdout.write(dumps(hosts[args.host]) if args.host in hosts
+													 else "{}")
+		return 0
+
+	except Exception as Ex:
+		sys.stderr.write("error: %s" % Ex)
+		return 1
+
+
+if __name__ == "__main__":
+	sys.exit(main())
+
